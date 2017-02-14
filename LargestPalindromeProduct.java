84529089e0ea860b4c252d740fe44ec43eae7432
ATLowther <atlowther@icloud.com>
2017-02-13 22:01:22 -0500

    New problem solved

--- LargestPalindromeProduct.java
+++ LargestPalindromeProduct.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class LargestPalindromeProduct {
 
@@ -14,17 +14,50 @@ public class LargestPalindromeProduct {
 
 	}
 	
+	public static int isLargest(int tester, int tester2)
+	{
+		if (tester < 999)
+		{
+			tester += 1;
+		}
+		if (tester2 < 999)
+		{
+			tester2 += 1;
+		}
+		List<Integer> palin = new ArrayList<Integer>();
+		for (int a = tester; a < 1000; a++) 
+		{
+			for (int b = tester2; b < 1000; b++)
+			{
+				int c = b * a;
+				if (isPalindrome(c))
+				{
+					palin.add(c);					
+				}
+			}
+		}
+		System.out.print(palin.size());
+		int d = palin.get(palin.size() - 1);	
+		return d;
+	}
+	
+	
+	
 	public static int threeDigitProduct() 
 	{
 		for (int x = 999; x > 0; x--) 
 		{
 			for (int y = 999; y > 0; y--) 
 			{
-				return ((int)x * y);
+				int z = y * x;
+				if (isPalindrome(z))
+				{
+					int k = (isLargest(y, x));		
+					return k;
+				}
 			}
-		}
+		}	
 		return 0;
-			
 	}
 	
 	public static boolean isPalindrome (int test) 
---
+++ SmallestMultiple.java
@@ -0,0 +1,72 @@
+
+public class SmallestMultiple {
+
+	public static void main(String[] args) {
+		
+		for (int a = 20; ; a += 2)
+		{
+			if (a % 1 == 0) 
+			{
+				if (a % 2 == 0) 
+				{
+					if (a % 3 == 0) 
+					{
+						if (a % 4 == 0) 
+						{
+							if (a % 5 == 0) 
+							{
+								if (a % 6 == 0) 
+								{
+									if (a % 7 == 0) 
+									{
+										if (a % 8 == 0) 
+										{
+											if (a % 9 == 0) 
+											{
+												if (a % 10 == 0) 
+												{
+													if (a % 11 == 0) 
+													{
+														if (a % 12 == 0) 
+														{
+															if (a % 13 == 0) 
+															{
+																if (a % 14 == 0) 
+																{
+																	if (a % 15 == 0) 
+																	{
+																		if (a % 16 == 0) 
+																		{
+																			if (a % 17 == 0) 
+																			{
+																				if (a % 18 == 0) 
+																				{
+																					if (a % 19 == 0) 
+																					{
+																						if (a % 20 == 0) 
+																						{
+																							System.out.print(a);
+																							break;
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
